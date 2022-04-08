f0b093db3f33df8860f9e44891398dfa54d11681
Flow <flow@ms188.com>
2022-04-08 18:10:01 +0800

    update: init podlib

---
+++ Resource/hellosdk/static/release/HelloSDK.framework/Headers/HelloSDK.h
@@ -0,0 +1,18 @@
+//
+//  HelloSDK.h
+//  HelloSDK
+//
+//  Created by Flow on 4/8/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloSDK.
+FOUNDATION_EXPORT double HelloSDKVersionNumber;
+
+//! Project version string for HelloSDK.
+FOUNDATION_EXPORT const unsigned char HelloSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloSDK/PublicHeader.h>
+
+
